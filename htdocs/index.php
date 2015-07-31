06869118bafba066cf25bda8200e8f7c8a851ddf
Kathryn Reeve <Kathryn@binaryKitten.com>
2015-07-31 14:06:52 +0100

    ZFDemo commit

---
+++ htdocs/index.php
@@ -0,0 +1,20 @@
+<?php
+chdir(dirname(__DIR__));
+require 'vendor/autoload.php';
+
+$app = \ZeffMu\App::init();
+$app
+    ->route('/', function () {
+        return '<a href="/hello">HEllo!</a>';
+    })
+    ->route('/hello', function () {
+        return 'Hi!';
+    })
+//    ->route('/array_route', array(ArrayRouteController::class, 'method'))
+    ->route('/hello/:name', function ($params) use ($app) {
+        return 'Hello ' . $params['name'];
+    })
+    ->route('/hello/:name/:surname', function ($params) use ($app) {
+        return 'Hello, Mr. ' . $params['surname'] . ', or shall I call you ' . $params['name'] . '?';
+    })
+    ->run();
